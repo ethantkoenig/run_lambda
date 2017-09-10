d08842acb939b6f1a75675a307031c2c7be0374e
Ethan Koenig <ethantkoenig@gmail.com>
2017-09-09 21:54:02 -0700

    Fix version numbers

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.rst"), "r") as f:
 
 setup(
     name="run_lambda",
-    version="0.1.8",
+    version="0.1.7.2",
     description="Run AWS Lambda functions locally",
     long_description=long_description,
     url="https://github.com/ethantkoenig/run_lambda",
