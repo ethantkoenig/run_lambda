ed0275ac5e5ab46a170d859941cea6d9e7197d0d
Ethan Koenig <ethantkoenig@gmail.com>
2017-09-09 21:46:39 -0700

    Version 0.1.7.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.rst"), "r") as f:
 
 setup(
     name="run_lambda",
-    version="0.1.7",
+    version="0.1.8",
     description="Run AWS Lambda functions locally",
     long_description=long_description,
     url="https://github.com/ethantkoenig/run_lambda",
